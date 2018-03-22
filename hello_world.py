fbf5ccc615670a0190514b763032549062020201
Robert Freeborn <robertfreeborn2@gmail.com>
2018-03-22 11:29:00 -0400

    I don't know what I'm doing.

--- hello_world.py
+++ hello_world.py
@@ -4,8 +4,4 @@ desires = input("Should I? \n")
 if desires.lower() == "yes":
 	print("Hello world.")
 else:
-<<<<<<< HEAD
 	print("Chris is a booty.  However, I'm messing this up again.")
-=======
-	print("I'm changing this to test my git repository functionality again.")
->>>>>>> parent of e69dc7e... Chris is a booty.
