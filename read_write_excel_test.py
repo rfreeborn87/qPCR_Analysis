8d83072a7679302947fff3b8d784375076eb46c6
Robert Freeborn <robertfreeborn2@gmail.com>
2018-03-22 12:32:04 -0400

    Read excel and write to new file

--- read_write_excel_test.py
+++ read_write_excel_test.py
@@ -1,38 +1,20 @@
 #In this script, I simply want to read an file from the ABI7500 and re-write it as a new file.
 
-#this block of text is how I would read a txt file
-#f = open("E:/OneDrive/Research/Rockwell_Lab/Python/092017_Influenza_Diet_Secondary_IFNg_Blimp-1_RF.txt","r")
-#print((f.read())
-#f.close()
-
-"""#I think I need pandas to read excel?
-import pandas as pd
-xl = pd.ExcelFile("E:/OneDrive/Research/Rockwell_Lab/Python/092017_Influenza_Diet_Secondary_IFNg_Blimp-1_RF.xls")
-#Can add sheet names, but my file has none.  Will test this feature out later.
-xl.sheet_names
-    [u'Sheet1']
-
-df = xl.parse("Sheet1")
-df.head()"""
-
 #For files with only one sheet_names
 import pandas as pd
 df = pd.read_excel("E:/OneDrive/Research/Rockwell_Lab/Python/092017_Influenza_Diet_Secondary_IFNg_Blimp-1_RF.xls")
-print(df.head())
+#print(df.head())
+#To facilitate working with multiple sheets from the same file, the ExcelFile class can be used to wrap the file and can be passed into read_excel
+#xlsx = pd.ExcelFile('path_to_file.xls')
+#df = pd.read_excel(xlsx, 'Sheet1')
 
-"""#Alternative method
-from openpyxl import load_workbook
-wb2 = load_workbook('E:/OneDrive/Research/Rockwell_Lab/Python/092017_Influenza_Diet_Secondary_IFNg_Blimp-1_RF.xls')
-print(wb2.get_sheet_names())
-['Sheet2', 'New Title', 'Sheet1']
+#To write a new excel file
+#df.to_excel('path_to_file.xlsx', sheet_name='Sheet1')
+df.to_excel("E:/OneDrive/Research/Rockwell_Lab/Python/092017_Influenza_Diet_Secondary_IFNg_Blimp-1_RF.xlsx",sheet_name="Cool")
 
-worksheet1 = wb2['Sheet1'] # one way to load a worksheet
-worksheet2 = wb2.get_sheet_by_name('Sheet2') # another way to load a worksheet
-print(worksheet1['D18'].value)
-for row in worksheet1.iter_rows():
-    print(row[0].value())"""
+"""#In order to write separate DataFrames to separate sheets in a single Excel file, one can pass an ExcelWriter.
+with ExcelWriter('path_to_file.xlsx') as writer:
+    df1.to_excel(writer, sheet_name='Sheet1')
+    df2.to_excel(writer, sheet_name='Sheet2')"""
 
-#new_file = open("E:/OneDrive/Research/Rockwell_Lab/Python/rewritten_data.xls","w")
-#new_file.write(str(f.read()))
-#new_file.close()
-#f.close()
+#For more information, see the pandas tutorial at http://pandas-docs.github.io/pandas-docs-travis/io.html#writing-excel-files
