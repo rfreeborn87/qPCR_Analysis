fc3382e0c5ba08aa0475adf36e5ec19da6be5bb5
Robert Freeborn <robertfreeborn2@gmail.com>
2018-03-22 11:37:42 -0400

    Make a script to read excel files.

---
+++ read_write_excel_test.py
@@ -0,0 +1,9 @@
+#In this script, I simply want to read an file from the ABI7500 and re-write it as a new file.
+f = open("E:/OneDrive/Research/Rockwell_Lab/Python/092017_Influenza_Diet_Secondary_IFNg_Blimp-1_RF.xls","r")
+print((f.read())
+f.close()
+
+#new_file = open("E:/OneDrive/Research/Rockwell_Lab/Python/rewritten_data.xls","w")
+#new_file.write(str(f.read()))
+#new_file.close()
+#f.close()
